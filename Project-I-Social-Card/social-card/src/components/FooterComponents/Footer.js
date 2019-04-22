ffe2b27155a867ef7e52da394281fdbd31904c2f
Ming Liu <mliu@macpro.home>
2019-04-22 18:25:56 -0400

    add functionality to share and heart footer icons with counter

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -2,19 +2,35 @@ import React from 'react';
 import './Footer.css';
 
 const ContentFooter = () => {
+    let setHeartCount = 0;
+    let setShareCount = 0;
+    const updateHeart = () => {
+        setHeartCount++;
+        let heartIcon = document.querySelector(".fa-heart");
+        document.getElementById("heartCount").textContent = setHeartCount;
+        heartIcon.setAttribute("class","fas fa-heart i-active")
+    }
+
+    const updateShare = () => {
+        setShareCount++;
+        let shareIcon = document.querySelector(".fa-sync-alt");
+        document.getElementById("shareCount").textContent = setShareCount;
+        shareIcon.classList.add("i-active");
+    }
+
     return (
         <div className="contentFooter">
             <div className="footerIcon">
                 <i className="far fa-comment"></i>
-                <div className="iconCounter" id="shareCount"></div>
+                <div className="iconCounter"></div>
             </div>
             <div className="footerIcon">
-                <i className="fas fa-sync-alt"></i>
-                <div className="iconCounter" id="shareCount">6</div>
+                <i className="fas fa-sync-alt" onClick = {updateShare}></i>
+                <div className="iconCounter" id="shareCount">{setShareCount}</div>
             </div>
             <div className="footerIcon">
-                <i className="far fa-heart"></i>
-                <div className="iconCounter" id="heartCount">4</div>
+                <i className="far fa-heart" onClick = {updateHeart} ></i>
+                <div className="iconCounter" id="heartCount">{setHeartCount}</div>
             </div>          
                 <div className="footerIcon">
                 <i className="far fa-envelope"></i>
