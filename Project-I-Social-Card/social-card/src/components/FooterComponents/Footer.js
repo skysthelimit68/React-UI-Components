bb0af176340c3e668d2489816253e4f00b816ca1
Ming Liu <mliu@macpro.home>
2019-04-22 16:14:05 -0400

    social card mvp + partial footer (no function)

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -4,9 +4,18 @@ import './Footer.css';
 const ContentFooter = () => {
     return (
         <div class="contentFooter">
-            <div class="footerIcon"><i class="far fa-comment"></i></div>
-            <div class="footerIcon"><i class="fas fa-sync-alt"></i></div>
-            <div class="footerIcon"><i class="far fa-heart"></i></div>          
+            <div class="footerIcon">
+                <i class="far fa-comment"></i>
+                <div class="iconCounter" id="shareCount"></div>
+            </div>
+            <div class="footerIcon">
+                <i class="fas fa-sync-alt"></i>
+                <div class="iconCounter" id="shareCount">6</div>
+            </div>
+            <div class="footerIcon">
+                <i class="far fa-heart"></i>
+                <div class="iconCounter" id="heartCount">4</div>
+            </div>          
             <div class="footerIcon"><i class="far fa-envelope"></i></div>
         </div>
     )
